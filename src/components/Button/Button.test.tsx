e82b32fcb8a9fd7b6ea68d779f6559c68572f865
Daphne Smit <daphnesmit1986@gmail.com>
2021-10-05 14:56:05 +0200

    chore: fix tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,46 +1,51 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'tests/test-utils';
+import { render, screen } from 'tests/test-utils';
 
 import { Button } from './Button';
 
 describe('Button', () => {
   it('large renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" />, div);
+    render(<Button label="Button text" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('medium renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" size="medium" />, div);
+    render(<Button label="Button text" size="medium" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('small renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" size="small" />, div);
+    render(<Button label="Button text" size="small" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('large renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" size="large" />, div);
+    render(<Button label="Button text" size="large" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('primary renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" variant="primary" />, div);
+    render(<Button label="Button text" variant="primary" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('secondary renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button label="Button text" variant="secondary" />, div);
+    render(<Button label="Button text" variant="secondary" />);
+
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 
   it('renders a backgroundColor when send', () => {
-    const button = render(<Button label="Button text" backgroundColor="red" />);
+    render(<Button label="Button text" backgroundColor="red" />);
 
-    expect(button.getByRole('button')).toHaveStyle('background-color: red');
-    expect(button.getByRole('button')).toHaveTextContent('Button text');
+    expect(screen.getByRole('button')).toHaveStyle('background-color: red');
+    expect(screen.getByRole('button')).toHaveTextContent('Button text');
   });
 });
